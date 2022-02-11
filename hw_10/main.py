a6d3675d4d175ff881c87652976cddb38d61d0a7
Maxim <99118327+Maximz2@users.noreply.github.com>
2022-02-11 17:24:58 +0300

    Modify

--- hw_10/main.py
+++ hw_10/main.py
@@ -7,16 +7,40 @@ from flask import Flask
 FILE_IMPORT = "candidates.json"
 
 
-def data_import(file_json):
-    with open(file_json, encoding="utf-8") as f:
-        candidates_list = json.load(f)
-    return candidates_list
-
 app = Flask(__name__)
 
+
 @app.route("/")
-def page_index(candidates_list):
+def page_main():
+    stroka = ''
+    for i in range(len(candidates)):
+        stroka += f"<pre>{candidates[i]['name']}\n" \
+                  f"{candidates[i]['position']}\n" \
+                  f"{candidates[i]['skills']}</pre>\n"
+    return stroka
 
-    return "Главная страница"
 
-app.run()
+@app.route("/candidate/<int:index>")
+def page_candidate(index):
+    stroka = f"<img src='{candidates[index]['picture']}'>\n" \
+             f"<pre>{candidates[index]['name']}\n" \
+             f"{candidates[index]['position']}\n" \
+             f"{candidates[index]['skills']}</pre>\n"
+    return stroka
+
+
+@app.route("/skills/<skill>")
+def page_skill(skill):
+    stroka = ''
+    for i in range(len(candidates)):
+        if skill in candidates[i]['skills'].lower():
+            stroka += f"<pre>{candidates[i]['name']}\n" \
+                      f"{candidates[i]['position']}\n" \
+                      f"{candidates[i]['skills']}</pre>\n"
+    return stroka
+
+
+if __name__ == '__main__':
+    with open(FILE_IMPORT, encoding="utf-8") as f:
+        candidates = json.load(f)
+    app.run()
